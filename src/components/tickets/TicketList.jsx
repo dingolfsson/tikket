3aa785e4a2daf826f74c5bb1731873438f65aa40
dingolfsson <dai5@hi.is>
2019-01-21 18:30:21 +0000

    Table moved from render:

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -44,79 +44,87 @@ class TicketList extends Component {
     this.props.history.push("/ticket/" + id);
   }
 
+  renderTable = (data) => {
+    const { column, direction } = this.state;
+    return (
+      <Table color="orange" sortable compact fixed singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell
+              width={3}
+              sorted={column === 'authorFirstName' ? direction : null}
+              onClick={this.handleSort('authorFirstName')}
+            >
+              Nafn
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              width={1}
+              sorted={column === 'selectedOption' ? direction : null}
+              onClick={this.handleSort('selectedOption')}
+            >
+
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              width={5}
+              sorted={column === 'title' ? direction : null}
+              onClick={this.handleSort('title')}
+            >
+              Lýsing
+              </Table.HeaderCell>
+            <Table.HeaderCell
+              width={3}
+              textAlign='right'
+              sorted={column === 'phone' ? direction : null}
+              onClick={this.handleSort('phone')}
+            >
+              Sími
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              width={3}
+              textAlign='right'
+              sorted={column === 'created' ? direction : null}
+              onClick={this.handleSort('created')}
+            >
+              Stofnað
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data.map(ticket => {
+            return (
+              <Table.Row key={ticket.id} positive={ticket.solved} warning={ticket.priority && !ticket.solved} onClick={() => this.handleClick(ticket.id)}>
+                <Table.Cell>{ticket.name}</Table.Cell>
+                <Table.Cell>
+                  <Icon name={ticket.selectedOption} />
+
+                </Table.Cell>
+                <Table.Cell>{ticket.priority && !ticket.solved ? <Icon name={'attention'} /> : null}{ticket.title}</Table.Cell>
+                <Table.Cell textAlign='right'>{ticket.phone}</Table.Cell>
+                <Table.Cell textAlign='right'>{moment(ticket.createdAt.toDate()).locale('is').calendar()}</Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table>
+    )
+  }
+
   // render: function
   // @return jsx
   render() {
-    const { column, data, direction } = this.state
+    const { data } = this.state
 
     // Condition: while data is undefined, a loader is displayed.
-    if (data === undefined) {
+    if (!data) {
       return <Loader />
     }
+
     return (
       <Container>
         <Button.Group labeled icon color='blue' floated='right'>
           <Button as={Link} to='/ticket/new' labelPosition='left' icon='plus' content='Ný beiðni' />
         </Button.Group>
-        <Table color="orange" sortable compact fixed singleLine>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell
-                width={3}
-                sorted={column === 'authorFirstName' ? direction : null}
-                onClick={this.handleSort('authorFirstName')}
-              >
-                Nafn
-            </Table.HeaderCell>
-              <Table.HeaderCell
-                width={1}
-                sorted={column === 'selectedOption' ? direction : null}
-                onClick={this.handleSort('selectedOption')}
-              >
-
-              </Table.HeaderCell>
-              <Table.HeaderCell
-                width={5}
-                sorted={column === 'title' ? direction : null}
-                onClick={this.handleSort('title')}
-              >
-                Lýsing
-              </Table.HeaderCell>
-              <Table.HeaderCell
-                width={3}
-                textAlign='right'
-                sorted={column === 'phone' ? direction : null}
-                onClick={this.handleSort('phone')}
-              >
-                Sími
-            </Table.HeaderCell>
-              <Table.HeaderCell
-                width={3}
-                textAlign='right'
-                sorted={column === 'created' ? direction : null}
-                onClick={this.handleSort('created')}
-              >
-                Stofnað
-            </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {data.map(ticket => {
-              return (
-                <Table.Row key={ticket.id} positive={ticket.solved} warning={ticket.priority && !ticket.solved} onClick={() => this.handleClick(ticket.id)}>
-                  <Table.Cell>{ticket.name}</Table.Cell>
-                  <Table.Cell>
-                    <Icon name={ticket.selectedOption} />
-
-                  </Table.Cell>
-                  <Table.Cell>{ticket.priority && !ticket.solved ? <Icon name={'attention'} /> : null}{ticket.title}</Table.Cell>
-                  <Table.Cell textAlign='right'>{ticket.phone}</Table.Cell>
-                  <Table.Cell textAlign='right'>{moment(ticket.createdAt.toDate()).locale('is').calendar()}</Table.Cell>
-                </Table.Row>
-              )
-            })}
-          </Table.Body>
-        </Table>
+        {this.renderTable(data)}
       </Container>
     )
   }
